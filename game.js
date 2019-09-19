b33e75c0850ea376eb9decf9dc59e75893b1d8ef
xander <ximart79@harrisonburg.k12.va.us>
2019-09-19 09:45:04 -0400

    make it better

--- game.js
+++ game.js
@@ -1,15 +1,31 @@
-var score = 0;
+// var score = 0;
 
-postToPage(score); // Print to the page
+// postToPage(score); // Print to the page
 
-createButton('Button', btnPress); // Make a button
-changeTitle('Clicker Game');
+// createButton('Button', btnPress); // Make a button
+// changeTitle('Clicker Game');
 
-setInterval(btnPress, 1000);
+// setInterval(btnPress, 1000);
+
+// function btnPress() {
+//   score++;
+//   postToPage(score);
+// }
+
+var score = 0; // Create a variable to store the score in
+
+var title = new Title('Welcome to the Game'); // Create a title element
+var button = new Button('hello', btnPress);         // Create a button element
+var scr = new Text(score);                     // Create a text element
+var button1 = new Button('hello', btnPress2);
+
 
 function btnPress() {
-  score++;
-  postToPage(score);
+  score++;         // Increase the score
+  scr.edit(score); // Update the page with the new score
 }
 
-createButton('Button', btnPress); 
+function btnPress2() {
+  score--;         // decrease the score
+  scr.edit(score); // Update the page with the new score
+}
