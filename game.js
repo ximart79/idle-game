d809f69e3a789311cf2742bdb6c9c885cd171c8d
xander <ximart79@harrisonburg.k12.va.us>
2019-09-23 08:44:41 -0400

    Update game.js

--- game.js
+++ game.js
@@ -15,9 +15,9 @@
 var score = 0; // Create a variable to store the score in
 
 var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);         // Create a button element
+var button = new Button('eyeballs', btnPress);         // Create a button element
 var scr = new Text(score);                     // Create a text element
-var button1 = new Button('hello', btnPress2);
+var button1 = new Button('coins', btnPress2);
 
 
 function btnPress() {
@@ -32,6 +32,8 @@ function btnPress() {
 }
 
 function btnPress2() {
-  score--;         // decrease the score
+  score++;         // decrease the score
+  var scr = new Text(score);   
   scr.edit(score); // Update the page with the new score
+    var score = 0; // Create a variable to store the score in
 }
